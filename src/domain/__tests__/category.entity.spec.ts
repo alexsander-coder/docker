a347665ec459ea8ae141d4533632988aa87dd6c8
Alexsander Coder <alexsander-hugo@hotmail.com>
2024-05-31 19:47:40 +0000

    test init category

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -1,3 +1,47 @@
-test('xpto', () => {
-  expect(1 + 2).toBe(3)
+import { Category } from "../category.entity"
+
+describe('category unit tests', () => {
+  describe('constructor', () => {
+    test('constructor create category with default values', () => {
+      const category = new Category({
+        name: 'movie'
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('movie');
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("shold create a category with all values", () => {
+      const created_at = new Date()
+      const category = new Category({
+        name: 'movie',
+        description: 'description movie',
+        is_active: true,
+        created_at
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('movie');
+      expect(category.description).toBe('description movie');
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBe(created_at);
+
+    })
+
+    test("constructor all 3", () => {
+      const created_at = new Date()
+      const category = new Category({
+        name: 'movie',
+        description: 'description movie',
+        is_active: false,
+        created_at
+      })
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe('movie');
+      expect(category.description).toBe('description movie');
+      expect(category.is_active).toBeFalsy();
+      expect(category.created_at).toBe(created_at);
+    })
+  })
 })
